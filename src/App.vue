ae04db7882c45c0b7a1e2297896e80c62fbf8a1f
MagicSakuraD <ckgoforit@outlook.com>
2023-04-06 23:14:39 +0800

    make a form

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,97 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, computed, watch } from "vue";
+// const todos = ref([]);
+// const name = ref("");
+// const input_content = ref("");
+// const output_category = ref([]);
+
+// const todos_asc = computed(() =>
+//   todos.value.sort((a, b) => {
+//     return b.createdAt - a.createdAt;
+//   })
+// );
+
+// 创建响应式变量
+const todos = ref([]); // 待办事项列表
+const name = ref(""); // 用户名
+const input_content = ref(""); // 输入框内容
+const input_category = ref([]); // 输ru分类
+// 根据创建时间升序排列的待办事项列表
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
+const addTodo = () => {
+  if (input_content.value.trim() === "" || input_category.value === null) {
+    return;
+  }
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime(),
+  });
+};
+watch(
+  todos,
+  (newValue) => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  },
+  { deep: true }
+);
+
+//watch函数接收两个参数，第一个参数是要监视的变量名，第二个参数是一个回调函数，用于在变量发生变化时执行一些操作。在这段代码中，回调函数会将新值存储在浏览器的本地存储中，这样即使用户关闭浏览器，值也会被保存下来，并在下次打开时自动加载。
+watch(name, (newVal) => {
+  localStorage.setItem(name, newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
 </script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        你好,<input type="text" placeholder="你的名字" v-model="name" />
+      </h2>
+    </section>
+    <section class="create-todo">
+      <h3>创建一个待办事项列表</h3>
+      <form @submit.prevent="addTodo">
+        <h4>你的待办事项列表上有什么？</h4>
+        <input
+          type="text"
+          placeholder="例如:制作一个视频"
+          v-model="input_content"
+        />
+        <h4>选择一个类别</h4>
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="business"
+              v-model="input_category"
+            />
+            <span class="bubble business"></span>
+            <div>工作</div>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="personal"
+              v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>个人</div>
+          </label>
+        </div>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+        <input type="submit" value="添加待办事项" />
+      </form>
+    </section>
+  </main>
+</template>
